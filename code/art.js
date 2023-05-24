3913a51735f038dabfbb429d367fcfdfb3d12a17
Aritra Bera <111326696+Edge24@users.noreply.github.com>
2023-05-24 09:27:30 +0530

    2.0
    Grand

---
+++ code/art.js
@@ -0,0 +1,119 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+
+//Reload
+const refreshButton = document.getElementById('refreshButton');
+
+refreshButton.addEventListener('click', function() {
+    location.reload();
+});
+
+
+
+//Right click button//
+
+//Events for desktop and touch
+let events = ["contextmenu", "touchstart"];
+//initial declaration
+var timeout;
+//for double tap
+var lastTap = 0;
+//refer menu div
+let contextMenu = document.getElementById("context-menu");
+
+//same function for both events
+events.forEach((eventType) => {
+  document.addEventListener(
+    eventType,
+    (e) => {
+      e.preventDefault();
+      //x and y position of mouse or touch
+      let mouseX = e.clientX || e.touches[0].clientX;
+      let mouseY = e.clientY || e.touches[0].clientY;
+      //height and width of menu
+      let menuHeight = contextMenu.getBoundingClientRect().height;
+      let menuWidth = contextMenu.getBoundingClientRect().width;
+      //width and height of screen
+      let width = window.innerWidth;
+      let height = window.innerHeight;
+      //If user clicks/touches near right corner
+      if (width - mouseX <= 200) {
+        contextMenu.style.borderRadius = "16px 16px 16px 16px";
+        contextMenu.style.left = width - menuWidth + "px";
+        contextMenu.style.top = mouseY + "px";
+        //right bottom
+        if (height - mouseY <= 200) {
+          contextMenu.style.top = mouseY - menuHeight + "px";
+          contextMenu.style.borderRadius = "16px 16px 16px 16px";
+        }
+      }
+      //left
+      else {
+        contextMenu.style.borderRadius = "16px 16px 16px 16px";
+        contextMenu.style.left = mouseX + "px";
+        contextMenu.style.top = mouseY + "px";
+        //left bottom
+        if (height - mouseY <= 200) {
+          contextMenu.style.top = mouseY - menuHeight + "px";
+          contextMenu.style.borderRadius = "16px 16px 16px 16px";
+        }
+      }
+      //display the menu
+      contextMenu.style.visibility = "visible";
+    },
+    { passive: false }
+  );
+});
+
+//for double tap(works on touch devices)
+document.addEventListener("touchend", function (e) {
+  //current time
+  var currentTime = new Date().getTime();
+  //gap between two gaps
+  var tapLength = currentTime - lastTap;
+  //clear previous timeouts(if any)
+  clearTimeout(timeout);
+  //if user taps twice in 500ms
+  if (tapLength < 500 && tapLength > 0) {
+    //hide menu
+    contextMenu.style.visibility = "hidden";
+    e.preventDefault();
+  } else {
+    //timeout if user doesn't tap after 500ms
+    timeout = setTimeout(function () {
+      clearTimeout(timeout);
+    }, 500);
+  }
+  //lastTap set to current time
+  lastTap = currentTime;
+});
+
+//click outside the menu to close it (for click devices)
+document.addEventListener("click", function (e) {
+  if (!contextMenu.contains(e.target)) {
+    contextMenu.style.visibility = "hidden";
+  }
+});
+
+
+
+//terms popup
+var maxOpenBtn = document.getElementById("maxOpenBtn");
+var maxPopup = document.getElementById("maxPopup");
+
+maxOpenBtn.addEventListener("click", function() {
+  maxPopup.style.display = "block";
+});
+
+document.addEventListener("click", function(event) {
+  if (event.target.id === "maxCloseBtn") {
+    maxPopup.style.display = "none";
+  }
+});
